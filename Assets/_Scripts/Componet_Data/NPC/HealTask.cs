2738e7bf77d5c8b1434eb6b0b928b060b38df1a4
wbeldenb <wbeldenb@calpoly.edu>
2018-05-29 01:56:33 -0700

    Added Task Scripts
    -Added 2 super simple tasks: healing and changing the players money

---
+++ Assets/_Scripts/Componet_Data/NPC/HealTask.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealTask : MonoBehaviour {
+    public void healPlayer(int healingAmount) {
+        GameObject player = GameObject.FindWithTag("Player");
+        player.GetComponent<PlayerController>().heal(healingAmount);
+    }
+}
---
+++ Assets/_Scripts/Componet_Data/NPC/PaymentTask.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaymentTask : MonoBehaviour {
+    public void payPlayer(int paymentAmount) {
+        GameObject player = GameObject.FindWithTag("Player");
+        player.GetComponent<PlayerController>().changeMoney(paymentAmount);
+    }
+}
